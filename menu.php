e63aa61136f1270b9808ff3c41d5dd33a697a62b
FaezehShahmirzaei <112942929+FaezehShahmirzaei@users.noreply.github.com>
2023-03-06 17:16:26 +0330

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,46 @@
+<?php
+$register_script = "./add to favorites.htm";
+?>
+<html dir="rtl">
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<meta http-equiv="Content-Language" content="en-us">
+<meta name="ProgId" content="FrontPage.Editor.Document">
+<meta name="GENERATOR" content="Microsoft FrontPage 6.0">
+
+
+<title>?? ??#1740; |??#1740;?| ???? | ??/title&gt; &lt;/head&gt; &lt;body&gt; &lt;/body&gt; &lt;/html&gt;
+</title>
+<script language="JavaScript">
+
+var bookmarkurl="http://www.jahad.com/"
+var bookmarktitle="jahad"
+
+function ccaddbookmark() {
+	if (document.all)
+	window.external.AddFavorite(bookmarkurl,bookmarktitle)
+}
+
+function mailsome1(){
+who=prompt("Enter recipient's email address: ","delta@yahoo.com");
+what=prompt("Enter the subject: ","test");
+if (confirm("Are you sure you want to mail "+who+" with the subject of "+what+"?")==true){
+window.location.href='mailto:'+who+'?subject='+what+'';
+   }
+}
+</script>
+
+<table border="0" width="100%" id="table1">
+	<tr>
+		<td width="100">&nbsp;</td>
+		<td>
+		<span lang="fa"><font color="#336699"><a href="start.php">&#1589;&#1601;&#1581;&#1607; &#1575;&#1589;&#1604;&#1740; </a>&nbsp;|&nbsp;<a href="registermlek.php">&#1593;&#1590;&#1608;&#1740;&#1578; </a>&nbsp;|&nbsp;<a href="login.php"> &#1579;&#1576;&#1578; &#1605;&#1604;&#1705;</a>&nbsp;|&nbsp;<a href="login.php"> &#1580;&#1587;&#1578;&#1580;&#1608; </a>&nbsp;|&nbsp;<a href='javascript:mailsome1()'> &#1575;&#1585;&#1587;&#1575;&#1604; &#1662;&#1740;&#1575;&#1605;</a> |<a href="login.php"> &#1608;&#1585;&#1608;&#1583; 
+		&#1576;&#1607; &#1587;&#1575;&#1740;&#1578; </a>|&#1583;&#1585;&#1576;&#1575;&#1585;&#1607; &#1605;&#1575; | &#1606;&#1602;&#1588;&#1607; &#1587;&#1575;&#1740;&#1578;</span>&nbsp;|&nbsp;<a class=chlnk style="CURSOR: hand; font-decoration: underline" onclick="this.style.behavior='url(#default#homepage)';this.setHomePage('http://www.delta.com');" href="./"><span lang="fa">&#1579;&#1576;&#1578; &#1583;&#1585; </span>home&nbsp;page <span lang="fa"> &nbsp;</a>|&nbsp; </span><a href="javascript:ccaddbookmark()" target="_self">
+		<span lang="fa">&#1579;&#1576;&#1578; &#1583;&#1585;</span>favorites</a></td>
+	</tr>
+</table>
+</head>
+<body>
+</body>
+</html>
